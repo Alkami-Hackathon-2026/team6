87598112756255847007bae53e2ccd36bd36a7e1
Alex Shiplett <E900103@texanscu.org>
2026-09-24 00:25:24 -0500

    updated postbuild.ps1 to custom, to update nuspec version and sem.ver version on build

--- HACK26.Client.Widget.MyMoneyRules/HACK26.Client.Widget.MyMoneyRules/Tools/PostBuild.ps1
+++ HACK26.Client.Widget.MyMoneyRules/HACK26.Client.Widget.MyMoneyRules/Tools/PostBuild.ps1
@@ -21,7 +21,7 @@ Write-Output "PostBuild initiated for $projectName";
 
 cd $projectDir;
 
-$expectedWorkingPath = (Join-Path $projectDir 'HACK26.Client.Widget.MyMoneyRules.nuspec')
+$expectedWorkingPath = (Join-Path $projectDir 'TXNSCU.Client.Widget.LAW.nuspec')
 
 if (!(Test-Path $expectedWorkingPath)) {
 	$expectedWorkingPath = (Get-ChildItem (Join-Path $projectDir "*.nuspec") | Sort LastWriteTime | Select -First 1).FullName;
@@ -31,6 +31,61 @@ $versionPrefix = '1.0.0'
 ## Write-Output "Looking for version to pack with";
 
 $semverPath = (Join-Path $projectDir "sem.ver")
+
+
+## Code to update the version files:
+
+    $projectFilePath = Join-Path $projectDir "$projectName.csproj"
+    $nuspecPath = Join-Path $projectDir "$projectName.nuspec"
+    $assemblyFilePath = Join-Path $projectDir "Properties\AssemblyInfo.cs"
+    $pattern = '^\[assembly: AssemblyVersion\("(.*)"\)\]'
+    $version = "1.0.0.0"
+    ## Write-Output "Getting project file from $projectFilePath"
+    [xml]$xml = Get-Content $projectFilePath
+    ## Write-Output "Getting target framework"
+    $targetFramework = $xml.Project.PropertyGroup.TargetFramework
+    ## Write-Output "Target framework is $targetFramework"
+    if($targetFramework -eq "net8.0"){
+        Write-Output "Getting version from project file"
+        $version = [Version] $xml.Project.PropertyGroup.AssemblyVersion
+    } else {
+        Write-Output "Getting version from AssemblyInfo.cs"
+        (Get-Content $assemblyFilePath) | ForEach-Object{
+        if($_ -match $pattern){
+            # We have found the matching line
+            # Edit the version number and put back.
+            $version = [version]$matches[1]
+            }
+        }
+    }
+
+    Write-Output "Read version as:"
+    Write-Output $version.ToString()
+
+
+    Write-Output "Creating sem.ver object"
+    # Create a custom object with the desired JSON structure
+    $semVersion = [ordered]@{
+        Version = [ordered]@{
+            Major = $version.Major
+            Minor = $version.Minor
+            Patch = $version.Build
+        }
+    }
+    $semVersionOutput = $semVersion | ConvertTo-Json -Depth 2
+    $semVersionOutput = $semVersionOutput -replace "  "," "
+    Write-Output "Updating sem.ver"
+    ## Write-Output $semVersionOutput
+    [xml] $nuspecFile = Get-Content $nuspecPath
+    $nuspecFile.Package.Metadata.Version = $version.Major.ToString() + "." + $version.Minor.ToString() + "." + $version.Build.ToString()
+
+    Write-Output "Updating .nuspec version"
+    ## Write-Output $nuspecFile.Package.Metadata.Version
+
+    $semVersionOutput | Set-Content -Path $semverPath
+    $nuspecFile.Save($nuspecPath)
+
+## end code
 
 if (!(Test-Path $semverPath)) {
     $semverPath = (Join-Path (Split-Path $projectDir) "sem.ver")
